9967177c3cedae5da8bc44133359c26e95cfe39c
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2019-01-06 01:59:47 -0500

    Dev: Code changes done for animation

--- Matlab Code/LQR.m
+++ Matlab Code/LQR.m
@@ -55,16 +55,48 @@ outputs = {'x'; 'phi1'; 'phi2'};
 sys_cl = ss(Ac,Bc,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
 % hold on
 x0=[2,0,0.3,0,0.3,0]  %% initial condition
-t = 0:0.01:100;
-r =10*ones(size(t)); %% Step input 
+t = 0:0.1:100;
+r =8*ones(size(t)); %% Step input 
 [y,t,x]=lsim(sys_cl,r,t,x0); %% simulation command
-figure
-[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot'); %% Plot of x and Theta 1
-set(get(AX(1),'Ylabel'),'String','cart position (m)')
-set(get(AX(2),'Ylabel'),'String','pendulum 1 angle (radians)')
- figure %% new figure
- 
-[AX,H1,H3] = plotyy(t,y(:,1),t,y(:,3),'plot');  %% Plot of x and Theta 2
-set(get(AX(1),'Ylabel'),'String','cart position (m)')
-set(get(AX(2),'Ylabel'),'String','pendulum 2 angle (radians)')
-title('Step Response with LQR Control')
+% figure
+% [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot'); %% Plot of x and Theta 1
+% set(get(AX(1),'Ylabel'),'String','cart position (m)')
+% set(get(AX(2),'Ylabel'),'String','pendulum 1 angle (radians)')
+%  figure %% new figure
+%  
+% [AX,H1,H3] = plotyy(t,y(:,1),t,y(:,3),'plot');  %% Plot of x and Theta 2
+% set(get(AX(1),'Ylabel'),'String','cart position (m)')
+% set(get(AX(2),'Ylabel'),'String','pendulum 2 angle (radians)')
+% title('Step Response with LQR Control')
+%%
+pend1x=-5*sin(x(:,3))+x(:,1);
+pend1y=-5*cos(x(:,3));
+pend2x=-10*sin(x(:,5))+x(:,1);
+pend2y=-10*cos(x(:,5));
+hold on;
+axis([-5 5 -15 5]);
+axis('square');
+grid on;
+length= length(t);
+hbead=line(x(1,1),t(1),'color','r','marker','square','markersize',30);
+
+line1x=[x(:,1),pend1x];
+line1y=[zeros(length,1),pend1y];
+hbead1=line(line1x(1),line1y(1),'marker','hexagram','markersize',5,'color','g');
+
+line2x=[x(:,1),pend2x];
+line2y=[zeros(length,1),pend2y];
+hbead2=line(line2x(1),line2y(1),'marker','hexagram','markersize',5);
+
+for k=2:length
+set(hbead,'xdata',x(k,1),'ydata',0);
+set(hbead1,'xdata',line1x(k,:),'ydata',line1y(k,:));
+set(hbead2,'xdata',line2x(k,:),'ydata',line2y(k,:));
+drawnow
+% F(k)=getframe(gcf);
+end
+
+% video=VideoWriter('123.avi','Uncompressed AVI');
+% open(video);
+% writeVideo(video,F);
+% close video
